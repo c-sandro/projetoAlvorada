afc4a83ebf7f29b2fd204c785fe527889221c15d
carlos sandro <csgonc400@gmail.com>
2023-09-27 17:58:03 -0300

    função de ganhar energia ao colocar o mouse no sol adicionado

---
+++ godot/scripts/HUD.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+
+var sunTime: float = 0
+@export var sunTimeRequired: int = 0
+
+var isMouseInSun: bool = false
+
+
+func _process(delta):
+	if(isMouseInSun):
+		sunTime += delta
+		if(sunTime >= sunTimeRequired):
+			updateSunAmount(1)
+			sunTime = 0
+		
+func updateSunAmount(sunAmount):
+	$SunCount.text = str(sunAmount + int($SunCount.text))
+
+func _on_sun_mouse_entered():
+	isMouseInSun = true
+
+func _on_sun_mouse_exited():
+	isMouseInSun = false
+	sunTime = 0
