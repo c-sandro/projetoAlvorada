769f9b1934b0906912f53068c6b6db5a6c122d83
carlos sandro <csgonc400@gmail.com>
2023-09-28 23:12:18 -0300

    otimização do codigo de ganhar sol

--- godot/scripts/SunEnergyManager.gd
+++ godot/scripts/SunEnergyManager.gd
@@ -1,22 +1,17 @@
 extends Node
 
-var sunTime: float = 0
-@export var sunTimeRequired: int = 1
-
-var isMouseInSun: bool = false
-
 var sunTotal: int = 0
 var sunGainedPerTick: int = 1
+var sunPerSecond: float = 0
 
-var upgradesCosts: Array = [10,50]
 var buildings: Array = [0]
+var upgradesCosts: Array = [10,50]
 
 signal sunTick
 signal upgradeBought
 
 func completeSunTick():
 	sunTotal += sunGainedPerTick
-	sunTime = 0
 	changeSunCountHUD()
 	
 func changeSunCountHUD():
@@ -25,22 +20,15 @@ func changeSunCountHUD():
 func changeUpgradePriceHUD(upgrade, newPrice):
 	upgradeBought.emit(upgrade, newPrice)
 
-func _process(delta):
-	
-	if(isMouseInSun):
-		sunTime += delta
-		if(sunTime >= sunTimeRequired):
-			completeSunTick()
-	
-
+#funções pra começar e parar o timer do sol
 func _on_sun_mouse_entered():
-	isMouseInSun = true
+	$SunTimer.start()
 
 func _on_sun_mouse_exited():
-	isMouseInSun = false
-	sunTime = 0
+	$SunTimer.stop()
 
 func _on_hud_mouse_upgrade_pressed():
+	
 	if(sunTotal >= upgradesCosts[0]):
 		sunTotal -= upgradesCosts[0]
 		sunGainedPerTick += 1
@@ -60,7 +48,9 @@ func _on_hud_building_buy_pressed():
 		if(buildings[0] == 1):
 			$BuildingTimer.start()
 
-
 func _on_building_timer_timeout():
 	sunTotal += 1
 	changeSunCountHUD()
+
+func _on_sun_timer_timeout():
+	completeSunTick()
