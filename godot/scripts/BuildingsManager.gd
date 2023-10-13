f58ff721394ed6b074125ea244f5680df1c512e8
c-sandro <124182880+c-sandro@users.noreply.github.com>
2023-10-13 11:24:23 -0300

    implementação de mais construções

--- godot/scripts/BuildingsManager.gd
+++ godot/scripts/BuildingsManager.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var buildings: Array = [Timer]
+@export var buildings: Array = [Timer, Timer, Timer]
 @export var sunEnergyManager: Node
 
 signal buildingTick
@@ -9,6 +9,10 @@ func _ready():
 	
 	buildings[0] = $WindPower/WindPowerTimer
 	buildings[0].startBuilding(25,1,10)
+	buildings[1] = $WaterPower/WaterPowerTimer
+	buildings[1].startBuilding(100,5,20)
+	buildings[2] = $NuclearPower/NuclearPowerTimer
+	buildings[2].startBuilding(250,10,15)
 
 func completeBuildingTimer(value):
 	buildingTick.emit(value)
@@ -20,7 +24,7 @@ func _on_wind_power_buy_button_down():
 	
 	sunEnergyManager.sunTotal -= buildings[0].cost
 	buildings[0].quantity += 1
-	buildings[0].cost = roundi(buildings[0].basePrice )#* (1.15 ** buildings[0].quantity))
+	buildings[0].cost = roundi(buildings[0].basePrice * (1.15 ** buildings[0].quantity))
 	
 	sunEnergyManager.changeSunCountHUD()
 
@@ -45,4 +49,69 @@ func _on_wind_power_buy_button_down():
 
 func _on_wind_power_timer_timeout():
 	completeBuildingTimer(buildings[0].sunGained * buildings[0].quantity)
+
+
+func _on_water_power_buy_button_down():
+	if(sunEnergyManager.sunTotal < buildings[1].cost):
+		return
 	
+	sunEnergyManager.sunTotal -= buildings[1].cost
+	buildings[1].quantity += 1
+	buildings[1].cost = roundi(buildings[1].basePrice * (1.15 ** buildings[1].quantity))
+	
+	sunEnergyManager.changeSunCountHUD()
+
+	sunEnergyManager.changeBuildingPriceHUD(1, buildings[1].cost)
+	
+	if(buildings[1].quantity == 1):
+		buildings[1].start()
+		$WaterPower/WaterPowerPlant.visible = true
+		$WaterPower/WaterPowerPlant.startBuilding()
+	elif(buildings[1].quantity == 10):
+		buildings[1].buildingMark1()
+	elif(buildings[1].quantity == 25):
+		buildings[1].buildingMark2()
+	elif(buildings[1].quantity == 50):
+		buildings[1].buildingMark3()
+	elif(buildings[1].quantity == 100):
+		buildings[1].buildingMark4()
+	
+	$WaterPower/WaterPowerPlant.updateBuilding(buildings[1].sunGained,
+								$WaterPower/WaterPowerTimer.wait_time,
+											  buildings[1].quantity)
+
+func _on_water_power_timer_timeout():
+	completeBuildingTimer(buildings[1].sunGained * buildings[1].quantity)
+
+func _on_nuclear_power_buy_button_down():
+	if(sunEnergyManager.sunTotal < buildings[2].cost):
+		return
+	
+	sunEnergyManager.sunTotal -= buildings[2].cost
+	buildings[2].quantity += 2
+	buildings[2].cost = roundi(buildings[2].basePrice * (1.15 ** buildings[2].quantity))
+	
+	sunEnergyManager.changeSunCountHUD()
+
+	sunEnergyManager.changeBuildingPriceHUD(2, buildings[2].cost)
+	
+	if(buildings[2].quantity == 2):
+		buildings[2].start()
+		$NuclearPower/NuclearPowerPlant.visible = true
+		$NuclearPower/NuclearPowerPlant.startBuilding()
+	elif(buildings[2].quantity == 10):
+		buildings[2].buildingMark2()
+	elif(buildings[2].quantity == 25):
+		buildings[2].buildingMark2()
+	elif(buildings[2].quantity == 50):
+		buildings[2].buildingMark3()
+	elif(buildings[2].quantity == 100):
+		buildings[2].buildingMark4()
+	
+	$NuclearPower/NuclearPowerPlant.updateBuilding(buildings[2].sunGained,
+								$NuclearPower/NuclearPowerTimer.wait_time,
+											  buildings[2].quantity)
+
+func _on_nuclear_power_timer_timeout():
+	completeBuildingTimer(buildings[2].sunGained * buildings[2].quantity)
+
